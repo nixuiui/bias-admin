34b761ddd7dd2b3b10da04c5acd6a4ee6b2671b2
nikisirka <niki@sirka.io>
2022-04-16 12:22:12 +0700

    fix(auth): fix bug auth auto logout

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,8 +1,6 @@
 import 'package:bias_admin/app/modules/home/controllers/home_controller.dart';
-import 'package:bias_admin/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:nx_flutter_ui_starter_pack/nx_flutter_ui_starter_pack.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({ Key? key }) : super(key: key);
@@ -15,14 +13,6 @@ class HomeView extends GetView<HomeController> {
         title: Text('Home'),
       ),
       body: Text('aaa'),
-      bottomNavigationBar: Container(
-        color: Colors.white,
-        padding: EdgeInsets.all(16),
-        child: NxButton.primary(
-          onPressed: () => AuthService.to.logout(),
-          child: NxText('Log Out', color: Colors.white),
-        ),
-      ),
     );
   }
 }
